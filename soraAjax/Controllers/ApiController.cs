7fe44cdb194e4cfd6259f71770bf27837ad7ba5c
sora2919 <sora2919@gmail.com>
2023-08-08 11:46:17 +0800

    GET方式傳遞參數

--- soraAjax/Controllers/ApiController.cs
+++ soraAjax/Controllers/ApiController.cs
@@ -9,5 +9,14 @@ namespace soraAjax.Controllers
             System.Threading.Thread.Sleep(5000); //睡5秒鐘，再往下執行
             return Content("Hello Ajax!");//Content是純文字
         }
+
+        public IActionResult GetDemo(string name,int age=30)//有設定值代表如果沒有輸入的話就預設值給他
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                name = "guest";
+            }
+            return Content($"Hello {name}! You're {age} years old.");
+        }
     }
 }
--- soraAjax/Controllers/HomeController.cs
+++ soraAjax/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace soraAjax.Controllers
             return View();
         }
 
+        public IActionResult GetDemo()
+        {
+            return View();
+        }
+
         public IActionResult Travel()
         {
             return View();
