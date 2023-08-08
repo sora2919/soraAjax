8a120d91fea9a4c93bbdf68498a719fea94b5291
sora2919 <sora2919@gmail.com>
2023-08-08 10:41:17 +0800

    Ajax Event - loadstart 及 loadend 執行圖示+防止使用者重複讀取

--- soraAjax/Controllers/ApiController.cs
+++ soraAjax/Controllers/ApiController.cs
@@ -6,6 +6,7 @@ namespace soraAjax.Controllers
     {
         public IActionResult Index()
         {
+            System.Threading.Thread.Sleep(5000); //睡5秒鐘，再往下執行
             return Content("Hello Ajax!");//Content是純文字
         }
     }
