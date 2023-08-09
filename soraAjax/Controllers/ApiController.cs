3bb25152341da28bd63d03df3ed12bdbbf4ed652
sora2919 <sora2919@gmail.com>
2023-08-09 15:30:38 +0800

    fetch()回傳的是promise物件

--- soraAjax/Controllers/ApiController.cs
+++ soraAjax/Controllers/ApiController.cs
@@ -22,6 +22,11 @@ namespace soraAjax.Controllers
             return Content("Hello Ajax!");//Content是純文字
         }
 
+        public IActionResult Fetch()
+        {
+            return Content("Hello Fetch!");
+        }
+
         public IActionResult GetDemo(UserViewModel user)//改成傳viewmodel
             //GetDemo(string name,int age=30)有設定值代表如果沒有輸入的話就預設值給他30
         {
